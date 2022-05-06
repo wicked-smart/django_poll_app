1695b40bec403d2a36615baf8df1b01e653207a0
wicked-smart <marzaanvan@gmail.com>
2022-05-06 12:23:51 +0530

    Add Vote and Results view

--- polls/urls.py
+++ polls/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('hello/<str:name>', views.say_name, name='name'),
-    path('details/<int:question_id>', views.details, name='details')
+    path('details/<int:question_id>', views.details, name='details'),
+    path('vote/<int:question_id>', views.vote, name="vote"),
+    path('result/<int:question_id>', views.result, name="result")
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,12 @@
-from django.http import HttpResponse
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import HttpResponse, render
 from .models import Question, Choice
 
 # Create your views here.
 
 def index(request):
-    q = Question.objects.order_by('-pub_date')[:3]
+    q = Question.objects.order_by('-pub_date')
     return render(request, 'polls/index.html',
     {
        "question_list": q 
@@ -31,6 +32,32 @@ def details(request, question_id):
     })
 
 def vote(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    try:
+        print(request.POST['choice'])
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError):
+        return render(request, "polls/details.html", 
+        {
+            "ques": question,
+            "error_message": "You didn't selected any option"
+        }
+        )
+    selected_choice.votes+=1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('result', args=(question.id,)))
+
+
+def result(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    return render(request, "polls/results.html", 
+        {
+           "question": question 
+        }
+    )
+
+
+
     
 
 
